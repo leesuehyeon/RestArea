df7d8833b28c9fc129514cc4f918e5665592653f
leesuehyeon <shsh010914@gmail.com>
2023-08-17 23:17:17 +0900

    서버 작업

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.125.55.121']
-# STATIC_ROOT = BASE_DIR / 'static/'
-# STATICFILES_DIRS = []
-# DEBUG = False
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
