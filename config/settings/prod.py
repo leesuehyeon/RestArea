823701d220cd9089bd3eef006ac0cc69c8a5c0db
leesuehyeon <shsh010914@gmail.com>
2023-08-18 00:18:49 +0900

    도메인 설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['52.79.214.192']
+ALLOWED_HOSTS = ['52.79.214.192', 'restarea.co.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
